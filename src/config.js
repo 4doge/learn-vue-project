1ec66eb5fa79752185017c4ac1b9d74fcc8ac8a9
Anton Boksha <antonboksha@gmail.com>
2018-09-22 08:58:01 +0300

    deploy json-server to heroku, update apiBaseUrl

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'production') {
   // possibility to split dev/prod envs
   apiBaseUrl = 'http://localhost:9999';
 } else {
-  apiBaseUrl = 'http://localhost:9999';
+  apiBaseUrl = 'https://stark-sea-84823.herokuapp.com';
 }
 
 export const apiHost = apiBaseUrl;
